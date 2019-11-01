0f4dd9fdd46c614b608ebf23faaf0db43b1ecb3f
Julian Andrade <julian@ndrade.com.br>
2019-11-01 16:07:23 -0300

    OpenWeather client

---
+++ src/app/service/open-weather-client.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class OpenWeatherClientService {
+
+	private get url() {
+		return 'http://api.openweathermap.org/data/2.5';
+	}
+
+	constructor(
+		private _http: HttpClient
+	) {
+	}
+
+	public getCurrent(lat, lon) {
+		const querystring = this.buildQuerystring({lat: lat, lon: lon});
+		return this._http.get(this.url + '/weather?' + querystring );
+	}
+
+	public getFiveDaysForecast(lat, lon) {
+		const querystring = this.buildQuerystring({lat: lat, lon: lon});
+		return this._http.get(this.url + '/forecast?' + querystring );
+	}
+
+	buildQuerystring(object) {
+		object.appid = environment.openWeatherApiKey;
+		object.units = 'metric'; // temperature units as celsius
+
+		return Object.entries(object).map(item => {
+			return `${item[0]}=${item[1]}`;
+		}).join('&');
+	}
+}
